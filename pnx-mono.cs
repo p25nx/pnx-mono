6ada3304c5affa9983d8491d660d3f78d78aaabb
David Krauss <nx4y@verizon.net>
2016-12-03 10:05:19 -0500

    Updating License

--- pnx-mono.cs
+++ pnx-mono.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+Copyright (C) 2015,2016,2017 David Krauss, NX4Y
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the 
+Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program;
+if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+LiteDB.dll is free software licensed under the MIT license. This is also included in the source tree.
+*/
+
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -713,7 +728,7 @@ namespace pnxmono
         }
         
         /* hit this timer when no more data has come in for x ms, assume that means remote is not transmitting any more */
-        public static void EOTCallback(object state)
+public static void EOTCallback(object state)
         {
             lock (locker)
             {
